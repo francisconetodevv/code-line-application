ce745162bac18cc3cad9e64b47e22735bd005288
Francisco <franciscoocontatoo@gmail.com>
2025-05-02 20:06:50 -0300

    Enviando projeto de linha de comando

---
+++ app/app.go
@@ -0,0 +1,69 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+/* Gerar vai retornar a aplicação de Linha de comando pronta para ser executada */
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de linha de comando"
+	app.Usage = "Busca de IPs e Nomes de Servidor na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na Internet",
+			Flags:  flags,
+			Action: buscarIPs,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIPs(c *cli.Context) {
+	host := c.String("host")
+
+	// Package net
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
+}
